fb6a076038c927c4e4056da2551328c2dbe0dd6e
Romron <romron@ukr.net>
2023-02-10 23:27:37 +0200

    Первичная структура плагина

--- wp-config.php
+++ wp-config.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Основные параметры WordPress.
  *
@@ -20,22 +21,22 @@
 
 // ** Параметры базы данных: Эту информацию можно получить у вашего хостинг-провайдера ** //
 /** Имя базы данных для WordPress */
-define( 'DB_NAME', 'dimaso-test' );
+define('DB_NAME', 'dimaso-test');
 
 /** Имя пользователя базы данных */
-define( 'DB_USER', 'root' );
+define('DB_USER', 'root');
 
 /** Пароль к базе данных */
-define( 'DB_PASSWORD', '' );
+define('DB_PASSWORD', '');
 
 /** Имя сервера базы данных */
-define( 'DB_HOST', 'localhost' );
+define('DB_HOST', 'localhost');
 
 /** Кодировка базы данных для создания таблиц. */
-define( 'DB_CHARSET', 'utf8mb4' );
+define('DB_CHARSET', 'utf8mb4');
 
 /** Схема сопоставления. Не меняйте, если не уверены. */
-define( 'DB_COLLATE', '' );
+define('DB_COLLATE', '');
 
 /**#@+
  * Уникальные ключи и соли для аутентификации.
@@ -48,14 +49,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         '=z5AoXB;CNt<?6*6V@f2GgN=!a=KkdD1RK5y@D[ycC`,MSif#mF>SugYiLIf:,a6' );
-define( 'SECURE_AUTH_KEY',  '_tuZy,kJs&xD& }7>*9oq`Pf:~+aUm m}g;w{:@g=-VxN/qmp$kvg0m$a<W6Rag%' );
-define( 'LOGGED_IN_KEY',    '8oz?#=.8]#*EzzdB-9fg^f]$PMJF[P6jaP1$?(NE+@W1NyQ]eMIk58C=U=1jsEKV' );
-define( 'NONCE_KEY',        'WD7u?F{hQO^[H%]Fb%52?u}@9$Vvej4,2~-<24zarFZ)8hIfPC[%@tpx};y.MUoM' );
-define( 'AUTH_SALT',        'Pm c1<e!nBD<3pKq=mI8{iE|C7y#JUEPkru(60GH]xpT$5ZKjM`o$r<F?ImXc*]P' );
-define( 'SECURE_AUTH_SALT', 'P450t7WXVw:zuH[RY_ k+IHm!Z~O5V@c[&rZ^05W6x=qHKoI=bR`/[gdtDNcQ]h3' );
-define( 'LOGGED_IN_SALT',   'Pi|G2n3?[iMNoDR*d|pRmS?woRKcN{v>/Bud)~*D+.4=>.?W *[{A;+vvr$}JoM.' );
-define( 'NONCE_SALT',       'Dz7lY__X[8h1:bE@Q|_[Ji7+jP%4L|50y(n*d5[BktDjgi=gG!p%)1hMf&V//w^@' );
+define('AUTH_KEY',         '=z5AoXB;CNt<?6*6V@f2GgN=!a=KkdD1RK5y@D[ycC`,MSif#mF>SugYiLIf:,a6');
+define('SECURE_AUTH_KEY',  '_tuZy,kJs&xD& }7>*9oq`Pf:~+aUm m}g;w{:@g=-VxN/qmp$kvg0m$a<W6Rag%');
+define('LOGGED_IN_KEY',    '8oz?#=.8]#*EzzdB-9fg^f]$PMJF[P6jaP1$?(NE+@W1NyQ]eMIk58C=U=1jsEKV');
+define('NONCE_KEY',        'WD7u?F{hQO^[H%]Fb%52?u}@9$Vvej4,2~-<24zarFZ)8hIfPC[%@tpx};y.MUoM');
+define('AUTH_SALT',        'Pm c1<e!nBD<3pKq=mI8{iE|C7y#JUEPkru(60GH]xpT$5ZKjM`o$r<F?ImXc*]P');
+define('SECURE_AUTH_SALT', 'P450t7WXVw:zuH[RY_ k+IHm!Z~O5V@c[&rZ^05W6x=qHKoI=bR`/[gdtDNcQ]h3');
+define('LOGGED_IN_SALT',   'Pi|G2n3?[iMNoDR*d|pRmS?woRKcN{v>/Bud)~*D+.4=>.?W *[{A;+vvr$}JoM.');
+define('NONCE_SALT',       'Dz7lY__X[8h1:bE@Q|_[Ji7+jP%4L|50y(n*d5[BktDjgi=gG!p%)1hMf&V//w^@');
 
 /**#@-*/
 
@@ -78,7 +79,7 @@ $table_prefix = 'dt_';
  *
  * @link https://ru.wordpress.org/support/article/debugging-in-wordpress/
  */
-define( 'WP_DEBUG', false );
+define('WP_DEBUG', true);
 
 /* Произвольные значения добавляйте между этой строкой и надписью "дальше не редактируем". */
 
@@ -87,8 +88,8 @@ define( 'WP_DEBUG', false );
 /* Это всё, дальше не редактируем. Успехов! */
 
 /** Абсолютный путь к директории WordPress. */
-if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', __DIR__ . '/' );
+if (!defined('ABSPATH')) {
+	define('ABSPATH', __DIR__ . '/');
 }
 
 /** Инициализирует переменные WordPress и подключает файлы. */
---
+++ wp-content/plugins/socer teams dt/socer teams dt.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: Socer teams dt
+Plugin URI: 
+Description: plugin for test 
+Version: 1.0
+Autor: Roman
+Autor URI: 
+Licene: GPLv2 or later
+Tex Domain: socer-teams-dt
+*/
+
+
+if (!defined('ABSPATH')) {
+   die;
+}
