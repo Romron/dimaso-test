96fd16fea72ec2fa95cd3cf4d6504125225c73be
Romron <Romron@ukr.net>
2023-02-13 09:13:54 +0200

    нормальный gitignore  для всего WP

--- wp-content/plugins/soccer teams dt/uninstall.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$arr_soccer_teams = get_post(array('post_type' => 'Soccer Team', 'numberposts' => -1));
-foreach ($arr_soccer_teams as $soccer_team) {
-   wp_delete_post($soccer_team->ID, true);
-}
