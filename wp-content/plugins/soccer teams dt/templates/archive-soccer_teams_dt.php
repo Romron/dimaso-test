32c4334d7c5d8fa0131b66c73314663f81d18ca1
Romron <romron@ukr.net>
2023-02-13 21:32:46 +0200

    что то

--- wp-content/plugins/soccer teams dt/templates/archive-soccer_teams_dt.php
+++ wp-content/plugins/soccer teams dt/templates/archive-soccer_teams_dt.php
@@ -3,7 +3,26 @@
 get_header(); ?>
 
 <div class="wraper">
-   **********************
+
+   <?php
+   if (have_posts()) {
+
+
+      while (have_posts()) {
+         the_post();
+
+
+   ?>
+
+   <?php }
+
+      // Pagination
+
+   } else {
+      echo '<p>' . esc_html__('No sosser teams', 'soccer-teams-dt') . '<p>';
+   }
+
+   ?>
 </div>
 
 <?php get_footer(); ?>
