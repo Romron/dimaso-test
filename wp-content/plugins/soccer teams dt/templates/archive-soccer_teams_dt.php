4081bb0ebc3e3dc45a8b0d1138adbe1c1524aac0
Romron <romron@ukr.net>
2023-02-13 21:59:39 +0200

    первичная вёрстка по разным файлам

--- wp-content/plugins/soccer teams dt/templates/archive-soccer_teams_dt.php
+++ wp-content/plugins/soccer teams dt/templates/archive-soccer_teams_dt.php
@@ -7,17 +7,14 @@ get_header(); ?>
    <?php
    if (have_posts()) {
 
-
       while (have_posts()) {
          the_post();
 
-
-   ?>
-
-   <?php }
+         $soccer_teams_dt_template_loader->get_template_part('partials\content');
+      }
 
       // Pagination
-
+      posts_nav_link();
    } else {
       echo '<p>' . esc_html__('No sosser teams', 'soccer-teams-dt') . '<p>';
    }
