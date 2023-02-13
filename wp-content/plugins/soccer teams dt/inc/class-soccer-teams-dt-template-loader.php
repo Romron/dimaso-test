a40496828582112aeddb068b36b7d1597c66d4f3
Romron <Romron@ukr.net>
2023-02-13 16:43:42 +0200

    что то

--- wp-content/plugins/soccer teams dt/inc/class-soccer-teams-dt-template-loader.php
+++ wp-content/plugins/soccer teams dt/inc/class-soccer-teams-dt-template-loader.php
@@ -9,26 +9,27 @@ class Soccer_Teams_dt_Template_loader extends Gamajo_Template_Loader
    protected $plugin_directory = SOCCERTEAMSDT_PATH;
    protected $plugin_template_directory = 'templates';
 
-   public function register(){
-      add_filter('template_include',[$this, 'soccer_teams_dt_template']);
+   public function register()
+   {
+      add_filter('template_include', [$this, 'soccer_teams_dt_template']);
    }
 
-   public function soccer_teams_dt_template($template){
- 
-      if(is_post_type_archive('soccer-team')){
+   public function soccer_teams_dt_template($template)
+   {
+
+      if (is_post_type_archive('soccer-team')) {
          $theme_files = ['archive-soccer_teams_dt.php', 'soccer_teams_dt/archive-soccer_teams_dt.php'];
          $exist = locate_template($theme_files, false);
-         if($exist != ''){
+         if ($exist != '') {
             return $exist;
-         }else{
-            return plugin_dir_path(__FILE__.'templates/archive-soccer_teams_dt.php')
+         } else {
+            return plugin_dir_path(__FILE__ . 'templates/archive-soccer_teams_dt.php');
          }
       }
 
       return $template;
    }
-
 }
 
 $soccer_teams_dt_template_loader = new Soccer_Teams_dt_Template_loader();
-$soccer_teams_dt_template_loader ->register(); 
+$soccer_teams_dt_template_loader->register();
